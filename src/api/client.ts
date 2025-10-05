3060b3a2297cefd080f91ab7a1da9a11fd928972
hub-lord <alimbayev.o@outlook.com>
2025-10-05 13:29:28 +0500

    update

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://ebankdxsxgjcniasaioy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViYW5rZHhzeGdqY25pYXNhaW95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NzI0MTcsImV4cCI6MjA3MzQ0ODQxN30.FiOVhiP5F6zheYRf0QEQN3dOvvCe_GQtK82Mz9MlgSg'
+const supabaseUrl = 'https://lhvcvrzqyyhdfympbwsr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxodmN2cnpxeXloZGZ5bXBid3NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1OTU5ODksImV4cCI6MjA3NTE3MTk4OX0.EMv4EtbXJdYv-BlvpysCnCH0xcqVjgQ643M7upJZsqQ'
 const supabase = createClient(supabaseUrl, supabaseKey)
 export { supabase }
