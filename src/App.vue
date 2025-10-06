b848ec6a344925f6b10217f59903cf86d3c4680e
hub-lord <alimbayev.o@outlook.com>
2025-10-06 19:28:33 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,12 @@ const { getLogin, getAuth } = storeToRefs(useMainStore())
 const miniApp = useMiniApp()
 const text = ref('load')
 const is401 = ref(false)
+const chatId = ref('')
 onMounted(() => {
   const userData = miniApp.initDataUnsafe;
-
+  console.log(userData.chat?.id);
+  localStorage.setItem("chat-id", userData.chat?.id?.toString())
+  chatId.value = userData.chat?.id?.toString() ?? 'test'
   setTimeout(() => {
     text.value = 'Нет доступа'
     is401.value = true
@@ -32,9 +35,11 @@ onMounted(() => {
 <template>
   <template v-if="getAuth">
     <a-page-header style="border: 1px solid rgb(235, 237, 240)" title="CRM" :sub-title="getLogin" />
+    {{ chatId }}
     <OrderList />
   </template>
   <template v-else>
+    {{ chatId }}
     <a-result v-if="is401" status="403" title="401" sub-title="Нет доступа">
     </a-result>
     <div style="display: flex; justify-content: center; align-items: center; align-content: center; height: 95vh;">
--- src/api/client.ts
+++ src/api/client.ts
@@ -1,5 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://lhvcvrzqyyhdfympbwsr.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxodmN2cnpxeXloZGZ5bXBid3NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1OTU5ODksImV4cCI6MjA3NTE3MTk4OX0.EMv4EtbXJdYv-BlvpysCnCH0xcqVjgQ643M7upJZsqQ'
-const supabase = createClient(supabaseUrl, supabaseKey)
+
+const supabase = createClient(supabaseUrl, supabaseKey, {
+    global: {
+      headers: {
+        'chat-id': localStorage.getItem("chat-id"), // в pre_request читаем как headers->>'x-chat-id'
+      },
+    },
+  })
 export { supabase }
